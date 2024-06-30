54c8d525edcc80c9c7923951f30fb7ae3128ff22
welove-t <tlove.tky@gmail.com>
2024-06-30 09:24:43 +0900

    🎨

--- _prettier.config.js
+++ _prettier.config.js
@@ -2,5 +2,7 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
+  semi: false,
+  singleQuote: true,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
